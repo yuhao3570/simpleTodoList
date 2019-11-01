7fdd2bff801670a7bbd943c805f580cdb209f289
Hao <Hao_Yu@epam.com>
2019-11-01 14:31:54 +0800

    adding frontend build files

---
+++ public/build/precache-manifest.52807511cb0f899dc9f471225124fdd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42437e5263947c62e0f79d82980bf75d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5744cbd4a918e0b79fc",
+    "url": "/static/css/main.f8a7aced.chunk.css"
+  },
+  {
+    "revision": "e33266f1367d29ff0a8f",
+    "url": "/static/js/2.e6305e8a.chunk.js"
+  },
+  {
+    "revision": "b5744cbd4a918e0b79fc",
+    "url": "/static/js/main.ef78b265.chunk.js"
+  },
+  {
+    "revision": "66ae3e21f60e33b29890",
+    "url": "/static/js/runtime-main.7d57867b.js"
+  }
+]);
